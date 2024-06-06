2e49eb39bfd50dde387bd72646f72523323d9698
Luis Barbosa <luisosb@hotmail.com>
2024-06-06 05:30:37 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember()
+{
+    var member = new Object();
+    member.name = "John";
+    member.age = 30;
+    member.skills = ["JavaScript", "React", "Angular"];
+
+    return member;
+}
